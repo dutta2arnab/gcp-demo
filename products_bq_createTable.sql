d0570e75409ec9c0e4f077f87da1825bff12eb55
dutta2arnab <47494602+dutta2arnab@users.noreply.github.com>
2023-04-04 11:50:17 +0530

    update project and dataset

--- products_bq_createTable.sql
+++ products_bq_createTable.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `products` (
+CREATE TABLE `bigquery-demo-for-dbt.mysql_retaildb_bq_python.products_from_github` (
   `product_id` integer,
   `product_category_id` integer,
   `product_name` string,
