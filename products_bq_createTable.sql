e9c7d316f3a285497cba9a3c92d215e2a5caa69d
dutta2arnab <47494602+dutta2arnab@users.noreply.github.com>
2023-04-04 11:09:02 +0530

    initial commit

---
+++ products_bq_createTable.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `products` (
+  `product_id` integer,
+  `product_category_id` integer,
+  `product_name` string,
+  `product_description` string,
+  `product_price` float64,
+  `product_image` string)
